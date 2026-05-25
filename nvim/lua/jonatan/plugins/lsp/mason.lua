10ef8aa96009d935dda5b96df78829f792b3f509
Jonatan Bengtsson <jonatan.bengtsson1994@gmail.com>
2026-05-25 10:43:36 +0200

    mason fix

--- nvim/lua/jonatan/plugins/lsp/mason.lua
+++ nvim/lua/jonatan/plugins/lsp/mason.lua
@@ -1,38 +1,40 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  config = function()
-    --import mason
-    local mason = require("mason")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		--import mason
+		local mason = require("mason")
 
-    --import mason-lspconfig
-    local mason_lspconfig = require("mason-lspconfig")
+		--import mason-lspconfig
+		local mason_lspconfig = require("mason-lspconfig")
 
-    local mason_tool_installer = require("mason-tool-installer")
+		local mason_tool_installer = require("mason-tool-installer")
 
-    mason.setup({})
+		mason.setup({})
 
-    mason_lspconfig.setup({
-      ensure_installed = {
-        "lua_ls",
-        "clangd",
-        "jedi_language_server",
-        "biome",
-        "html",
-        "cssls",
-      },
-    })
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"lua_ls",
+				"clangd",
+				"jedi_language_server",
+				"biome",
+				"html",
+				"cssls",
+				"gopls",
+				"postgres_lsp",
+			},
+		})
 
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "prettier",
-        "prettierd",
-        "stylua",
-        "eslint_d",
-      },
-    })
-  end,
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier",
+				"prettierd",
+				"stylua",
+				"eslint_d",
+			},
+		})
+	end,
 }
